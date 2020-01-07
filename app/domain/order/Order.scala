9b76f3240c81ba98b42eb7164bd7e38ca8e882dc
kazuki <pharcydetip@gmail.com>
2020-01-08 03:43:36 +0900

    add typ to order

--- app/domain/order/Order.scala
+++ app/domain/order/Order.scala
@@ -5,7 +5,8 @@ import domain.financialProduct.FinancialProduct
 /** order marker trait */
 case class Order(
     from: FinancialProduct, // product to be sold
-    to: FinancialProduct    // product to be bought
+    to: FinancialProduct,   // product to be bought
+    typ: OrderType
 ) {
   require(from.code != to.code)
 }
